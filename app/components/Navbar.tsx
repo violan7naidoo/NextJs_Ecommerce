f0774362a7f5f83239829bacb88ad6ad83a8fa16
violan7naidoo <violan7naidoo@gmail.com>
2024-03-08 11:29:55 +0200

    modulesfound

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "../../components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ShoppingBag } from "lucide-react";
