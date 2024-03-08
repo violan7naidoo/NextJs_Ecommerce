fc5f146bfee52f79c1b76c56c90d2dc346ecc4b5
violan7naidoo <violan7naidoo@gmail.com>
2024-03-08 11:33:20 +0200

    found one more

--- app/stripe/success/page.tsx
+++ app/stripe/success/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "../../../components/ui/button";
 import { CheckCheck } from "lucide-react";
 import Link from "next/link";
 
