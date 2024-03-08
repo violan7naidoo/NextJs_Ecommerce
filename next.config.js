d05b475d9b13c6df1b160252fae1a9e6b8197e66
violan7naidoo <76399629+violan7naidoo@users.noreply.github.com>
2024-03-08 09:15:07 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["cdn.sanity.io"],
   },
+  // other configuration options...
 };
 
 module.exports = nextConfig;
